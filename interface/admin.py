a904d426398e7a122dd7028cbed065b7ff467bd5
maheshgudi <mahesh.p.gudi@gmail.com>
2018-04-06 16:16:47 +0530

    Add admin feature to check and update all questions

--- interface/admin.py
+++ interface/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin, messages
-from interface.models import (Question, TestCase, StdIOBasedTestCase,
-                              Rating, Review)
 from django.contrib.auth.models import User, Group
 from django.contrib.auth.admin import GroupAdmin
+from interface.models import Question, TestCase, Rating, Review
+from interface.views import submit_to_code_server
 
 admin.site.register(Question)
 admin.site.register(TestCase)
@@ -42,6 +42,35 @@ class ReviewerAdmin(admin.ModelAdmin):
     make_reviewer.short_description = "Add Users in reviewer Group"
     remove_reviewer.short_description = "Remove Users from reviewer Group"
 
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["summary", "user", 'status']
+    actions = ["update_question_status"]
+
+    def update_question_status(self, request, questions):
+        try:
+            selected_questions = Question.objects.filter(id__in=questions)
+            for question in selected_questions:
+                result = submit_to_code_server(question.id)
+                if result.get("success"):
+                    question.status = True
+                    question.save()
+                else:
+                    question.status = False
+                    question.save()
+
+            messages.add_message(request, messages.SUCCESS,
+                                 "Question Status successfully updated."
+                                 )
+
+        except Exception as e:
+            messages.add_message(request, messages.ERROR,
+                                 'You have an error:\n {0}.'.format(e)
+                                 )
+
+    update_question_status.short_description = """Check and update
+                                                  question status"""
 
 admin.site.unregister(User)
 admin.site.register(User, ReviewerAdmin)
+admin.site.unregister(Question)
+admin.site.register(Question, QuestionAdmin)
